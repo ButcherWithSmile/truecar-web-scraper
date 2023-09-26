920f12491900cf4fc27cc18fd063585ad615a600
Hatef PourRajabi <131672914+ButcherWithSmile@users.noreply.github.com>
2023-09-26 16:52:23 +0330

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// This package uses the Colly web scraping library and GORM ORM to scrape car data from TrueCar and insert it into a MySQL database
+
 package main
 
 import (
@@ -9,6 +11,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// This struct defines the model for a car
 type CarInfo struct {
 	ID      int    `gorm:"primaryKey"`
 	Name    string `gorm:"column:name"`
@@ -17,16 +20,18 @@ type CarInfo struct {
 }
 
 func main() {
+	// This code prompts the user to enter the car brand and model
 	fmt.Println("Please enter the car brand:")
 	var carBrand, carModel string
 	fmt.Scanln(&carBrand)
 	fmt.Println("Please enter the model of car:")
 	fmt.Scanln(&carModel)
 
+	// This code creates a new Colly collector and a slice of CarInfo structs
 	c := colly.NewCollector()
-
 	var carInfo []CarInfo
 
+	//This code opens a connection to the MySQL database and creates a table for the CarInfo struct if it does not already exist
 	dsn := "username:password@tcp(localhost:3306)/db_name"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -38,6 +43,9 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//This code tells Colly to visit the specified URL and extract the car data from the .vehicle-card-body elements
+	// If the number of cars in the carInfo slice is less than 20, the code creates a new CarInfo struct from the extracted data and appends it to the slice
+	// The code then inserts the new car into the database
 	c.OnHTML(".vehicle-card-body", func(h *colly.HTMLElement) {
 		if len(carInfo) < 20 {
 			price := h.ChildText("span[data-test='vehicleListingPriceAmount']")
@@ -52,7 +60,9 @@ func main() {
 		}
 	})
 
+	// This code tells Colly to visit the TrueCar URL for the specified car brand and model
 	c.Visit("https://www.truecar.com/used-cars-for-sale/listings/" + carBrand + "/" + carModel + "/")
 
+	// This code prints a message to the user informing them that the scraping and insertion process is complete
 	fmt.Println("Scraping and inserting data into your database completed.")
 }
